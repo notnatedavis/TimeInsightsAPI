6c4f882b64a02f406989ff0464607df26167ccea
ndavispe <157775257+notnatedavis@users.noreply.github.com>
2025-08-18 09:56:22 -0400

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,18 @@
 // build configuration
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 4096,
+    sourcemap: true
+  }
+});
